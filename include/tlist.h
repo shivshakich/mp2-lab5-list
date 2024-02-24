d28b374bf7e430e0e3f6522ed14e8f81b204c706
VlaShi <moyoMylo>
2024-02-24 17:07:39 +0300

    write constructors for tlist

--- include/tlist.h
+++ include/tlist.h
@@ -15,12 +15,17 @@ protected:
 	TNode<T> * pCurr, * pPrev;
 	int pos;									// нумерация начинается с 0
 public:
+
+	// конструкторы, деструктор и операторы копирования
+
 	TList();
 	TList(const T& _val);
 	TList(const TList<T>& _list);
+	~TList();
 	TList<T>& operator=(const T& _val);
 	TList<T>& operator=(const TList<T>& _list);
-	~TList();
+
+	// втсавка или удаление звена
 
 	void InsFirst(const T& _val);
 	void InsLast(const T& _val);
@@ -28,16 +33,25 @@ public:
 	void DelFirst();
 	void DelCurr();
 
+	// методы доступа к pCurr
+
 	TNode<T>* GetCurr() const;							// доступ к pCurr значению
 	void SetPos(int _pos);								// установить значение pCurr в зависимости от позиция _pos
 	void Reset();										// установить pCurr на начало списка
 	void GoNext();										// перейти к следующему звену
-	bool IsEnd() const noexcept;					
+	bool IsEnd() const noexcept;
 
+	// другие методы
+					
 	void DelList();
+
+	// comparison operators
+
+	bool operator==(const TList<T>& _list) const;
+	bool operator!=(const TList<T>& _list) const;
 };
 
-// констркуторы 
+// CONSTRUCTORS & DESTRUCTOR
 
 template <class T>
 TList<T>::TList() : pStop(nullptr), pFirst(pStop), pLast(pStop), length(0), pCurr(pStop), pPrev(pStop), pos(-1) {}
@@ -51,8 +65,54 @@ TList<T>::TList(const T& _val) : pStop(nullptr), pCurr(pStop), pPrev(pStop), pos
 
 template<class T> 
 TList<T>::TList(const TList<T>& _list) {
-	pStop = _list.pStop;
+	this->pStop		= _list.pStop;
+	this->length	= _list.length;
+	this->pos		= _list.pos;
+	pPrev = pCurr = pStop;
 
-	int tmp = _list.length;
-	for (int i = 0; i < tmp; )
+	if (_list.pFirst == pStop) {
+		pFirst = pLast = pStop;
+		return;
+	}
+
+	pLast = pFirst = new TNode<T>;
+	pFirst->value = _list.pFirst->value;
+
+	TNode<T>* currNode = _list.pFirst->pNext;
+
+	while (currNode != pStop) {				// создаём список
+		TNode<T>* tmpNode = new TNode<T>;
+		tmpNode->value = currNode->value;
+		pLast->pNext = tmpNode;
+		pLast = tmpNode;
+	}
+	pLast->pNext = pStop;
+
+	// находим значения для pCurr, pPrev
+
+	// для случая pos == -1 уже найдены ранее pCurr, pPrev
+	
+	if (pos == 0) 			// для случая pos == 0
+		pCurr = pFirst;
+
+	// для случая pos >= 1 находим pPrev, по которому найдём pCurr = pPrev->pNext
+	else if (pos > 0) {
+		pPrev = pFirst;
+		int posPrev = pos - 1;
+		for (int i = 0; i < posPrev; ++i) {
+			pPrev = pPrev->pNext;
+		}
+		pCurr = pPrev->pNext;
+	}
+}
+
+template <class T>
+TList<T>::~TList() { DelList(); }
+
+// OPERATOR=
+
+template <class T>
+TList<T>& TList<T>::operator=(const T& _val) {
+	this->DelList();
+	this->InsLast(_val);
 }
--- test/test_tlist.cpp
+++ test/test_tlist.cpp
@@ -1,10 +1,15 @@
 #include "../gtest/gtest.h"
 #include "../include/tlist.h"
 
+// CONSTRUCTORS
+
 TEST(TList, can_create_list) {
-	ASSERT_NO_THROW(TList<double> l);
-	ASSERT_NO_THROW(TList<char> l(123));
+	ASSERT_NO_THROW(TList<double> l);			// default constructor
+	ASSERT_NO_THROW(TList<char> l(123));		// conversion constructor
+
+	TList<int> v;
 	TList<int> l(123);
+	ASSERT_NO_THROW(TList<int> list(v));		// copy constructor
 	ASSERT_NO_THROW(TList<int> list(l));
 }
 
@@ -14,7 +19,7 @@ TEST(TList, list_created_by_default_constructor_is_empty) {
 }
 
 TEST(TList, list_created_by_conversion_constructor_contains_one_vertex_and_has_correct_value) {
-	int n = 1234;
+	int n = 5976;
 	TList<int> list(n);
 
 	list.Reset();
@@ -26,12 +31,12 @@ TEST(TList, list_created_by_conversion_constructor_contains_one_vertex_and_has_c
 TEST(TList, list_filled_by_insert_methods_has_correct_values) {
 	int n1 = 65443, n2 = 56, n3 = -12, n4 = 8000;
 	TList<int> list(n2);
-	list.InsFirst(n1);
-	list.InsLast(n4);
+	list.InsFirst(n1);						// InsFirst
+	list.InsLast(n4);						// InsLast
 
 	list.Reset();
 	list.GoNext(); list.GoNext();
-	list.InsCurr(n3);
+	list.InsCurr(n3);						// InsCurr
 
 	ASSERT_TRUE(list.IsEnd());
 
@@ -43,16 +48,19 @@ TEST(TList, list_filled_by_insert_methods_has_correct_values) {
 }
 
 TEST(TList, list_created_by_copy_constructor_is_equivalent_to_original_one) {
-	TList<int> voidlist;
-	ASSERT_NO_THROW(TList<int> _list(voidlist));
-	TList<int> _list(voidlist);
+	TList<int> voidList;
+	TList<int> _list(voidList);
 	ASSERT_ANY_THROW(_list.DelFirst());
 
-	int n1 = 65443, n2 = 56, n3 = -12;
-	TList<int> original(n2);
-	original.InsFirst(n1); 
-	original.InsLast(n3);
+	int n1 = 93, n2 = 56, n3 = -2;
+	TList<int> originalList(n2);
+	originalList.InsFirst(n1); 
+	originalList.InsLast(n3);
 	// n1 n2 n3
 
-	ASSERT_NOT
+	TList<int> copyList(originalList);
+	ASSERT_TRUE(originalList == copyList);
 }
+
+// OPERATOR=
+
