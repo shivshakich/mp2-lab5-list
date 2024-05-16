b82061c7a3a6712ddf8aae48dcce6f415e4eb8e1
VlaShi <moyoMylo>
2024-05-17 00:40:51 +0300

    добавил кнопки [Ч]истки

--- sln/desktopapp/MyForm.h
+++ sln/desktopapp/MyForm.h
@@ -51,6 +51,8 @@ namespace CppWinForm1 {
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column1;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
 
 
 
@@ -82,6 +84,8 @@ namespace CppWinForm1 {
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -185,6 +189,26 @@ namespace CppWinForm1 {
 			this->Column2->ReadOnly = true;
 			this->Column2->Width = 121;
 			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(725, 13);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(38, 23);
+			this->button3->TabIndex = 9;
+			this->button3->Text = L"Ч";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(726, 39);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(37, 23);
+			this->button4->TabIndex = 10;
+			this->button4->Text = L"Ч";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
@@ -192,6 +216,8 @@ namespace CppWinForm1 {
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(192)),
 				static_cast<System::Int32>(static_cast<System::Byte>(255)));
 			this->ClientSize = System::Drawing::Size(882, 553);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->textBox4);
@@ -333,6 +359,14 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 	else {
 		textBox3->Text = "НЕПРАВИЛЬНАЯ_ОПЕРАЦИЯ"; return;
 	}
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	textBox1->Text = "";
+}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	textBox2->Text = "";
+	textBox3->Text = "";
+	textBox4->Text = "";
 }
 };
 }
